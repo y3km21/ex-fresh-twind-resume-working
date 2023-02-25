a173abe01edc2840f7dacf358819ce2bfcd6d3fb
y3km21 <y3km21@gmail.com>
2023-02-25 14:42:33 +0900

    Changed using twindv1 plugin

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,13 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { Options } from "$fresh-main-plugins/twindv1.ts";
+import { defineConfig } from "twind";
+
+import presetAutoPrefix from "twind-preset-autoprefix";
+import presetTailWind from "twind-preset-tailwind";
 
 export default {
+  ...defineConfig({
+    presets: [presetAutoPrefix(), presetTailWind()],
+  }),
+
   selfURL: import.meta.url,
 } as Options;
